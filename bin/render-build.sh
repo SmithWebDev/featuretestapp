7397ffe8628f864d6e5f2a92ae11ef12510fd8af
Darrick Smith <smithwebdd@gmail.com>
2022-10-27 12:52:21 -0400

    Created render-build script

---
+++ bin/render-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+
+# make script executable
+# chmod a+x bin/render-build.sh
