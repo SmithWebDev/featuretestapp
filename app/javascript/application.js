74dde7e99a2d0306149dc16f509aa6f7a5eaff73
Darrick Smith <smithwebdd@gmail.com>
2022-10-28 12:34:19 -0400

    Created custom console_log turbo stream

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import "@hotwired/turbo-rails";
+import "./controllers";
+
+import { StreamActions } from "@hotwired/turbo";
+
+StreamActions.console_log = function () {
+  const message = this.getAttribute("message");
+  console.log(message);
+};
